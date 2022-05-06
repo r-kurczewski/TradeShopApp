4b4f8c5d99f6da55b0d74d99c105565b7f6732cf
Radek Kurczewski <radek.kurczewski@gmail.com>
2022-05-06 13:46:15 +0200

    Add project files.

---
+++ TradeShopApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TradeShopApp.Models;
+
+namespace TradeShopApp.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+
+		}
+	}
+}
